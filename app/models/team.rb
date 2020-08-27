04524ea48836b476056de8f34aa1742946153718
Dan <dcox38@gmailL.com>
2020-08-27 00:30:50 -0500

    added team model that connects to player model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,29 @@
 class Team < ApplicationRecord
+  def pitcher
+    Player.find_by(id: pitcher_id) 
+  end
+  def catcher
+    Player.find_by(id: catcher_id) 
+  end
+  def first_base
+    Player.find_by(id: first_base_id) 
+  end
+  def second_base
+    Player.find_by(id: second_base_id) 
+  end
+  def third_base
+    Player.find_by(id: third_base_id) 
+  end
+  def shortstop
+    Player.find_by(id: shortstop_id) 
+  end
+  def left_field
+    Player.find_by(id: left_field_id) 
+  end
+  def center_field
+    Player.find_by(id: center_field_id) 
+  end
+  def right_field
+    Player.find_by(id: right_field_id) 
+  end
 end
