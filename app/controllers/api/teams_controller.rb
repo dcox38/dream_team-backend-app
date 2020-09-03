f6b95f3ce923224bf469440be0f0b43dacb9e59f
Dan <dcox38@gmailL.com>
2020-09-02 20:56:48 -0500

    teams index working again

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -3,11 +3,43 @@ class Api::TeamsController < ApplicationController
 
   def index
     @teams = current_user.teams
+    # @teams = Team.all
     render "index.json.jb"
   end
 
 
   def create
+
+    @total_war = 0
+
+    pitcher = Player.find_by(id: params[:pitcher])
+    pitcher.war
+    @total_war += pitcher.war 
+    catcher = Player.find_by(id: params[:catcher])
+    catcher.war
+    @total_war += catcher.war 
+    first_base = Player.find_by(id: params[:first_base])
+    first_base.war
+    @total_war += first_base.war 
+    second_base = Player.find_by(id: params[:second_base])
+    second_base.war
+    @total_war += second_base.war 
+    third_base = Player.find_by(id: params[:third_base])
+    third_base.war
+    @total_war += third_base.war 
+    shortstop = Player.find_by(id: params[:shortstop])
+    shortstop.war
+    @total_war += shortstop.war 
+    left_field = Player.find_by(id: params[:left_field])
+    left_field.war
+    @total_war += left_field.war 
+    center_field = Player.find_by(id: params[:center_field])
+    center_field.war
+    @total_war += center_field.war 
+    right_field = Player.find_by(id: params[:right_field])
+    right_field.war
+    @total_war += right_field.war 
+
     @team = Team.new(
 
       name: params[:name],
@@ -21,7 +53,7 @@ class Api::TeamsController < ApplicationController
       center_field_id: params[:center_field],
       right_field_id: params[:right_field],
       user_id: current_user.id,
-      total_war: params[:total_war]
+      total_war: @total_war
     )
 
     @team.save
